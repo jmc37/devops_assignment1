483845b5ee995373efa1b7992c943dc84aa4953f
Joseph Chan <josephchan36@gmail.com>
2023-09-23 12:35:23 -0700

    final

--- mygit-create-directory.sh
+++ mygit-create-directory.sh
@@ -5,5 +5,10 @@ if [ -z "$1" ]; then
     exit 1
 fi 
 
+if [ -e "$1" ]; then
+    echo "Directory already exists please provide a different name"
+    exit 1
+fi 
+
 mkdir "$1"
 echo "Directory created with name $1"
--- mygit-delete-file.sh
+++ mygit-delete-file.sh
@@ -2,6 +2,12 @@
 
 if [ -z "$1" ]; then
     echo "Please provide a file name"
+    exit 1
+fi
+
+if [ ! -e "$1" ]; then
+    echo "File does not exist"
+    exit 1
 fi
 
 rm "$1"
